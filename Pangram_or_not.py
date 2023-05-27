84eb374aed5a24415f76fdcf57581480734d04c6
niharika21P31A0467 <100333825+niharika21P31A0467@users.noreply.github.com>
2023-05-27 11:49:17 +0530

    l=input().lower().split() l=''.join(l) a='abcdefghijklmnopqrstuvwxyz' c=0 for i in a:     if i in l:         c=c+1 if c==len(a):     print("True") else:     print("False")

---
+++ Pangram_or_not.py
@@ -0,0 +1,11 @@
+l=input().lower().split()
+l=''.join(l)
+a='abcdefghijklmnopqrstuvwxyz'
+c=0
+for i in a:
+    if i in l:
+        c=c+1
+if c==len(a):
+    print("True")
+else:
+    print("False")
